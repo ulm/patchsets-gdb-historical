f77c6907c0ca7e1a6c8c54c13ce810803643ca22
Mike Frysinger <vapier@gentoo.org>
2012-08-07 20:15:17 +0000

    generalize script a bit with $PN/$PV variables

--- make-tarball.sh
+++ make-tarball.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-gver=$1
+PN="gdb"
+PV=$1
 pver=$2
 
-if [[ -z ${gver} ]] || [[ ! -d ./${gver} ]] ; then
-	echo "Usage: $0 <gdb ver> [patch ver]"
+if [[ -z ${PV} ]] || [[ ! -d ./${PV} ]] ; then
+	echo "Usage: $0 <${PN} ver> [patch ver]"
 	exit 1
 fi
 
 if [[ -z ${pver} ]] ; then
-	pver=$(awk '{print $1; exit}' ${gver}/README.history)
+	pver=$(awk '{print $1; exit}' ${PV}/README.history)
 	if [[ -z ${pver} ]] ; then
 		echo "need patch version"
 		exit 1
 	fi
 fi
 
-if grep -qs Header:.*gentoo ${gver}/*.patch ; then
+if grep -qs Header:.*gentoo ${PV}/*.patch ; then
 	echo "error: files were not added with cvs -kb"
-	grep Header:.*gentoo ${gver}/*.patch
+	grep Header:.*gentoo ${PV}/*.patch
 	exit 1
 fi
 
-tar=gdb-${gver}-patches-${pver}.tar.xz
+tar=${PN}-${PV}-patches-${pver}.tar.xz
 
 rm -rf tmp
 rm -f ${tar}
 
 mkdir -p tmp/patch
-cp ${gver}/*.patch tmp/patch/ || exit 1
+cp ${PV}/*.patch tmp/patch/ || exit 1
 mkdir -p tmp/extra
 cp `find extra -type f '!' -wholename '*/CVS/*'` tmp/extra/ || exit 1
 cp ../README* tmp/patch/
